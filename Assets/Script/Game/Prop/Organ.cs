b8dcfa42e77793c95cff9f440840c21d02465eaf
lixiongwen <997805135@qq.com>
2023-03-31 00:26:43 +0800

    内容：修改预制件

---
+++ Assets/Script/Game/Prop/Organ.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DLAM
+{
+    public class Organ : MonoBehaviour
+    {
+        public void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.transform.tag == "Player")
+            {
+                GameManager.Instance.EndGame();
+            }
+        }
+    }
+}
